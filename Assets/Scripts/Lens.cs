aad910105150d36fe7e6cdeaa01ada6a7de6f240
Max Linke <maxlinke@users.noreply.github.com>
2019-07-29 23:05:45 +0200

    Some progress and ideas

--- Assets/Scripts/Lens.cs
+++ Assets/Scripts/Lens.cs
@@ -4,19 +4,47 @@ using UnityEngine;
 
 public class Lens {
 
+    public const float MESH_RESOLUTION = 32;        //points per lens surface
+    public const float MIN_THICKNESS = 0.001f;      //1mm
+
     public float FocalLength => CalculateFocalLength();
 
-    float r1, r2, n, thickness, radius;
+    float r1, r2, n, d, r;
 
-    public Lens (float r1, float r2, float n, float thickness, float desiredRadius) {
-        if(TryGetIntersectPoint(r1, r2, thickness, out float x, out float y)){
-            Debug.Log($"yes: {x:F2}, {y:F2}");
-        }else{
-            Debug.Log("no");
+    public Lens (float frontRadius, float rearRadius, float refractiveIndex, float thickness, float lensRadius) {
+        this.r1 = frontRadius;
+        this.r2 = rearRadius;
+        this.d = Mathf.Max(MIN_THICKNESS, thickness);
+        this.n = refractiveIndex;
+        if(TryGetIntersectPoint(r1, r1, d, out float iX, out float iY)){        //TODO instead of r1, r2 and inferring stuff from there use a sphere/circle struct so i don't have to recalculate centers and offsets in multiple places...
+            lensRadius = Mathf.Min(Mathf.Abs(iY), lensRadius);
         }
     }
 
+    public Vector2[] GetFrontOutlinePointsBottomToTop () {
+        return null;
+    }
+
+    public Vector2[] GetBackOutlinePointsBottomToTop () {
+        return null;
+    }
+
+    public Vector2[] GetOutlinePoints () {
+        List<Vector2> output = new List<Vector2>();
+        var front = GetFrontOutlinePointsBottomToTop();
+        var back = GetBackOutlinePointsBottomToTop();
+        for(int i=0; i<front.Length; i++){
+            output.Add(front[i]);
+        }
+        for(int i=back.Length-1; i>=0; i--){
+            output.Add(front[i]);
+        }
+        return output.ToArray();
+    }
+
     public Mesh GetMesh () {
+        var front = GetFrontOutlinePointsBottomToTop();
+        var back = GetBackOutlinePointsBottomToTop();
         return null;
     }
 
@@ -61,7 +89,7 @@ public class Lens {
     }
 
     private float CalculateFocalLength () {
-        return 1f / ((n - 1f) * ((1 / r1) * (1 / r2) * (((n - 1) * thickness) / (n * r1 * r2))));
+        return 1f / ((n - 1f) * ((1 / r1) * (1 / r2) * (((n - 1) * d) / (n * r1 * r2))));
     }
 	
 }
